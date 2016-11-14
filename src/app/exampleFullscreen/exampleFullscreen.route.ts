af4a6ed228707ed2b26360bdb2843e0e66492f41
bertrandg <bertrandgaillard@hotmail.fr>
2016-11-14 22:59:43 +0100

    update example + TODO

--- src/app/exampleFullscreen/exampleFullscreen.route.ts
+++ src/app/exampleFullscreen/exampleFullscreen.route.ts
@@ -102,21 +102,21 @@ export class ExampleFullscreenRouteComponent {
   @ViewChild('logContainer') logContainer: ElementRef
   
   code: string = `
-<split direction="horizontal" [width]="splitWidth" [height]="splitHeight" (dragStart)="dragReport('start')" (drag)="dragReport('progress')" (dragEnd)="dragReport('end')">
+<split direction="horizontal" [width]="splitWidth" [height]="splitHeight" (dragStart)="dragReport('Split A|B|C start')" (drag)="dragReport('Split A|B|C progress')" (dragEnd)="dragReport('Split A|B|C end')">
     <split-area *ngIf="isA1 || isA2" size="25">
-        <split direction="vertical">
+        <split direction="vertical" (dragStart)="dragReport('Split A1|A2 start')" (drag)="dragReport('Split A1|A2 progress')" (dragEnd)="dragReport('Split A1|A2 end')">
             <split-area *ngIf="isA1" size="25">A1</split-area>
             <split-area *ngIf="isA2" size="75">A2</split-area>
         </split>
     </split-area>
     <split-area *ngIf="isB1 || isB2" size="50">
-        <split direction="vertical">
+        <split direction="vertical" (dragStart)="dragReport('Split B1|B2 start')" (drag)="dragReport('Split B1|B2 progress')" (dragEnd)="dragReport('Split B1|B2 end')">
             <split-area *ngIf="isB1" size="50">B1</split-area>
             <split-area *ngIf="isB2" size="50">B2</split-area>
         </split>
     </split-area>
     <split-area *ngIf="isC1 || isC2" size="25">
-        <split direction="vertical">
+        <split direction="vertical" (dragStart)="dragReport('Split C1|C2 start')" (drag)="dragReport('Split C1|C2 progress')" (dragEnd)="dragReport('Split C1|C2 end')">
             <split-area *ngIf="isC1" size="25">C1</split-area>
             <split-area *ngIf="isC2" size="75">C2</split-area>
         </split>
